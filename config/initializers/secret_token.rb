50934f6c869ccdf4e87ef921d690d885fb2d8edd
Nícolas Iensen <nicolas@quavio.com.br>
2012-04-11 13:48:36 -0300

    Rails template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TigerTracker::Application.config.secret_token = '06effd8f0564b0d0dad5a1108c3dd0648b9e72ea994ebd6eb26a14a67a0e7693d94eea57ca659b1360b7f175fcd31eaad9f5d2e9620992fbc9c59d053c3f7692'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TigerTracker::Application.config.session_store :cookie_store, key: '_tiger-tracker_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TigerTracker::Application.config.session_store :active_record_store
